33201df1427ff680879a22da7779b72b652f411a
Vasya <139713160+wert1cal120@users.noreply.github.com>
2024-07-09 10:11:12 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,4 +10,7 @@ counter.innerHTML = 0
 let button = document.getElementById("main-button");
 button.addEventListener("click", function(){
     counter.innerHTML = parseInt(counter.innerHTML)+1;
+    if ('vibrate' in navigator) {
+        navigator.vibrate(100);
+    }
 }); 
